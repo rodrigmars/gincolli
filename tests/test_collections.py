61eb85e04ea1ab225a4317b3f0a6cbc1b98cf731
rodrigmars <rodrigmars@gmail.com>
2023-01-03 12:13:05 -0300

    Novos testes incluidos

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -1,10 +1,25 @@
 from pytest import fixture
-from collections import Counter 
+from collections import Counter
 
 
 @fixture
-def setup():
+def setup() -> list[int]:
+
     return [5, 3, 5, 2, 1, 6, 6, 4]
 
-def test_find_duplicates(setup):
+
+def test_find_duplicates(setup: list[int]) -> None:
+
     assert Counter(setup).get(5) == 2
+
+
+def test_add(setup: list[int]) -> None:
+
+    assert Counter(setup).get(5, 0).__add__(2) == 4
+
+
+def test_median(setup: list[int]) -> None:
+
+    median = sum(setup) / setup.__len__()
+
+    assert median.__eq__(4)
